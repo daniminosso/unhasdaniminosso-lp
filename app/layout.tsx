e7c216bbfbe48997cfa8866da435518f5e6ea2e3
Daniele Minosso <daniminosso@gmail.com>
2025-11-26 17:44:11 -0300

    Instala Google Ads Conversion Tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,36 +93,9 @@ export default function RootLayout({
               })(window,document,'script','dataLayer','${gtm}');`}
             </Script>
             {/* fallback para caso JS esteja desabilitado */}
-            <noscript>
-              <iframe
-                src={`https://www.googletagmanager.com/ns.html?id=${gtm}`}
-                height="0"
-                width="0"
-                style={{ display: 'none', visibility: 'hidden' }}
-              />
-            </noscript>
-          </>
-        )}
-
-        {/* Google Analytics 4 */}
-        {ga4 && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${ga4}`}
-              strategy="afterInteractive"
-            />
-            <Script id="ga4-init" strategy="afterInteractive">
-              {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${ga4}', { page_path: window.location.pathname });
-              `}
-            </Script>
-          </>
-        )}{/* Google Ads (AW-16859467972) */}
+            {/* Google Ads – Conversões */}
 <Script
-  src="https://www.googletagmanager.com/gtag/js?id=AW-16859467972"
+  src={`https://www.googletagmanager.com/gtag/js?id=AW-16859467972`}
   strategy="afterInteractive"
 />
 
@@ -135,28 +108,8 @@ export default function RootLayout({
   `}
 </Script>
 
-
-        {/* Facebook Pixel */}
-        {fbPixel && (
-          <Script id="fb-pixel" strategy="afterInteractive">
-            {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${fbPixel}');
-            fbq('track', 'PageView');
-            `}
-          </Script>
-        )}
-
-        {/* UTMify */}
         {utmify && (
-          <>
+    <>
             {/* Inicializa a variável que o pixel.js usa */}
             <Script
               id="utmify-init"
